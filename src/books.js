6f5dae6736344bc0ae7c0bc8f2aa83f3a1be4a6d
TeamAbella060420 <jabella@rscm.com.sa>
2022-06-13 16:03:51 +0300

    Create books.js

---
+++ src/books.js
@@ -0,0 +1,20 @@
+export const books = [
+{
+  id: 1,
+  img: 'https://images-na.ssl-images-amazon.com/images/I/41KY-NORo9L._SY291_BO1,204,203,200_QL40_FMwebp_.jpg',
+  title: 'The 48 laws of power',
+  author: 'by Robert Greene',
+},
+{
+  id: 2,
+  img: 'https://images-na.ssl-images-amazon.com/images/I/51u8ZRDCVoL._SY291_BO1,204,203,200_QL40_FMwebp_.jpg',
+  title: 'Rich Dad Poor Dad',
+  author: 'by Robert T. Kiyosaki',
+},
+{
+  id: 3,
+  img: 'https://images-na.ssl-images-amazon.com/images/I/61y04z8SKEL._SY291_BO1,204,203,200_QL40_FMwebp_.jpg',
+  title: 'Think and Grow Rich',
+  author: 'by Napoleon Hill',
+},
+];
